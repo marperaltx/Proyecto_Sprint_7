ce00b8c80bbcc3e2b1142070cad7c3e692edd322
Mariana Peralta <marperaltamo@gmail.com>
2025-10-05 16:26:50 -0600

    Subir archivos a repositorio

---
+++ README.md
@@ -0,0 +1,8 @@
+# Proyecto_Sprint_7
+Creación de app web para el proyecto del Sprint 7
+
+Descripción del proyecto:
+ La aplicación esta diseñada para poder visualizar un histograma y un gráfico de dispersión sobre los anuncios de venta de coches.
+ Se consideran dos variables de los datos:
+  - Precio de los coches.
+  - Odómetro: dispositivo que mide la distancia total que ha recorrido un vehículo desde su fabricación.
---
+++ app.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+     
+car_data = pd.read_csv(r"C:\Users\marpe\OneDrive\Sprint 7\Proyecto Sprint 7\vehicles_us.csv") # leer los datos
+st.header('Anuncios de venta de coches')
+
+build_histogram = st.checkbox('Construir histograma') # crear un botón
+     
+if build_histogram: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+         
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+     
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+build_dispersion = st.checkbox("Construir gráfico de dispersión ") # crear un botón
+
+if build_dispersion: # al hacer clic en el botón
+    # escribir el mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    
+    # crear el gráfico de dispersión
+    fig_d = px.scatter(car_data, x="odometer", y="price")
+    
+    # mostrar un gráfico Plotli interactivo
+    st.plotly_chart(fig_d, use_container_width=True)
